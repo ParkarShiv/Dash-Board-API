242f5b3f181946a537a1ae1b0110bfdc54f522d9
Shivam <parkarshivam@gmail.com>
2025-06-02 16:21:28 +0530

    ...

--- XHR/DTOs/CreateLeaveRequestDto.cs
+++ XHR/DTOs/CreateLeaveRequestDto.cs
@@ -2,7 +2,7 @@
 {
     public class CreateLeaveRequestDto
     {
-        public string EmployeeFullName { get; set; }
+        public string EmployeeFullName { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Type { get; set; }
